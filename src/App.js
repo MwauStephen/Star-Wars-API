0b0067b723ccb685677deb224f8e4a9dada59586
Mwau stephen <stephenmwau41@gmail.com>
2022-05-28 22:14:25 +0800

    added the fetch api http request

--- src/App.js
+++ src/App.js
@@ -4,13 +4,31 @@ import "./App.css";
 import MoviesList from "./Components/MoviesList";
 
 const App = () => {
+  // sending HTTP Request
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/")
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        const transformedData = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+      });
+  }
   return (
     <Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movie />
       </section>
     </Fragment>
   );
