2b9e784d4c93ddca207e0d6cd555267f3a860352
Mwau stephen <stephenmwau41@gmail.com>
2022-05-28 23:54:08 +0800

    imported useEffect hook to handle the http request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./App.css";
 
 import MoviesList from "./Components/MoviesList";
