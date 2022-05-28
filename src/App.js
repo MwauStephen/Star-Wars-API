e08c0b19612e0a4c1c2658863ff6c8d19c795a37
Mwau stephen <stephenmwau41@gmail.com>
2022-05-28 22:41:11 +0800

    refactored the https request using async await

--- src/App.js
+++ src/App.js
@@ -7,27 +7,42 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   // sending HTTP Request
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        console.log(response);
+  // function fetchMoviesHandler() {
+  //   fetch("https://swapi.dev/api/films/")
+  //     .then((response) => {
+  //       console.log(response);
 
-        // convert into readable object
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        const transformedData = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedData);
-      });
-  }
+  //       // convert into readable object
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       const transformedData = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           releaseDate: movieData.release_date,
+  //         };
+  //       });
+  //       setMovies(transformedData);
+  //     });
+  // }
+
+  const fetchMoviesHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedData = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedData);
+  };
   return (
     <Fragment>
       <section>
